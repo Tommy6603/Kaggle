4464bb099d18b802b8873063832c668b89057103
Shoichiro Nagatomi <s.nagatomi0508@gmail.com>
2024-05-23 11:07:09 +0900

    240523

--- titanic/source/titanicNN.py
+++ titanic/source/titanicNN.py
@@ -6,42 +6,42 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-# データの読み込み
-data = pd.read_csv('../data/train.csv')
+# Read CSV
+data = pd.read_csv('./data/train.csv')
 
-# 必要な特徴量を選択
+# select feature values
 features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']
 X = data[features]
 y = data['Survived']
 
-# 欠損値の処理
+# Process missing values
 X['Age'].fillna(X['Age'].median(), inplace=True)
 X['Fare'].fillna(X['Fare'].median(), inplace=True)
 
-# カテゴリ変数のエンコーディング
+# Encoding Categorical variabl
 X = pd.get_dummies(X, columns=['Sex'], drop_first=True)
 
-# 訓練データとテストデータに分割
+# devide train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# データの標準化
-scaler = StandardScaler()
+# standardization of data
+scaler = StandardScaler() # x - μ / σ : removing mean and scaling unit variance
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# ニューラルネットワークモデルの構築
+# Buid the neural network model
 model = Sequential([
     Dense(32, input_dim=X_train.shape[1], activation='relu'),
     Dense(16, activation='relu'),
     Dense(1, activation='sigmoid')
 ])
 
-# モデルのコンパイル
+# compile model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# モデルの訓練
+# Train the model
 model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
 
-# モデルの評価
+# evaluate model
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f'Test Accuracy: {accuracy:.4f}')
